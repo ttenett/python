63b260a8a1fa798f08691d0d2e4eb9eb27b8b91b
ttenett <yeonjeongsdev@gmail.com>
2025-01-18 00:35:33 +0900

    파일 읽어오기 시도

---
+++ project/news/file_read.py
@@ -0,0 +1,14 @@
+import base64
+
+# article 파일 열기
+file = open('C:\\Users\\yeonjeong\\TIL\\01_python\\project\\news\\article', 'r')
+temp = file.read()
+file.close()
+print(temp)
+
+# html 파일열기
+file2 = open('news\summary.html', 'r', delcoding='CP949')
+file.close
+print(file2)
+
+
