b6c494791aadfda49fc3b5f69fe8a8d773cb1d2e
ttenett <yeonjeongsdev@gmail.com>
2025-01-21 23:34:57 +0900

    수정

--- project/news/file_read.py
+++ project/news/file_read.py
@@ -1,14 +1,19 @@
 import base64
+import codecs
 
-# article 파일 열기
-file = open('C:\\Users\\yeonjeong\\TIL\\01_python\\project\\news\\article', 'r')
-temp = file.read()
-file.close()
-print(temp)
+# # article 파일 열기
+# file = open('C:\\Users\\yeonjeong\\TIL\\01_python\\project\\news\\article', 'r')
+# temp = file.read()
+# file.close()
+# print(temp)
 
-# html 파일열기
-file2 = open('news\summary.html', 'r', delcoding='CP949')
-file.close
-print(file2)
+# # html 파일열기
+# file2 = open('news\summary.html', 'r', delcoding='CP949')
+# file.close
+# print(file2)
+
+# 파일에서 텍스트 읽기
+with codecs.open('C:\\Users\\yeonjeong\\TIL\\01_python\\project\\news\\article', 'r', encoding='utf-8') as f:
+    content = f.read()
 
 
