6e718456ce086d0da1e7bd457aa957feab705849
ttenett <yeonjeongsdev@gmail.com>
2025-02-12 21:17:13 +0900

    feat: git conflict 해결중

--- project/calender.py
+++ project/calender.py
@@ -1,6 +1,7 @@
 
 year = int(input('달력을 보고싶은 연도를 입력해주세요.'))
 month = int(input('달력을 보고싶은 월을 입력해주세요.'))
+leaf_year = 'false'
 
 # 입력받은 연도 윤년여부 판단
 def leaf_year(year):
@@ -11,8 +12,8 @@ def leaf_year(year):
         else:
             leaf_year = True
 
-# 입력받은 월의 날짜 판단 - 30, 31, 28
-def what_month(month):
+# 입력받은 월의 마지막 날짜 판단 - 30, 31, 28
+def what_lastday(month):
     match month:
         case 1, 3, 5, 7, 8, 10, 12:
             return 31
@@ -25,7 +26,7 @@ def what_month(month):
                 return 28
 
 # 달력 날짜 출력하기
-for i in range(1, what_month(month)+1):
+for i in range(1, what_lastday(month)):
     print(i)
 
 
